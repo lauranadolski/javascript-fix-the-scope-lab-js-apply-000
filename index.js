b0b77a0d9607e0580da78ada0dd10c63c6c255cd
Laura Nadolski <laura.nadolski@gmail.com>
2018-04-08 23:01:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 // Laura Nadolski
 // 4/8/18
 
-
 var animal = 'dog';
 
 function myAnimal() {
@@ -13,6 +12,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  animal = 'cat';
   return animal;
 }
 
